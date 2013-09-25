68a686768769d4170152cff3d9f838b68e243688
Pierre <pierrechevaliergeol@free.fr>
2013-09-25 23:24:39 +0200

    First words, and a loop.

--- frebol.r
+++ frebol.r
@@ -1,4 +1,16 @@
+#!/usr/bin/rebol -qs
 rebol []
-		##       \____/_____/ \___/_____/___/_____/__/_____/_/ |_/_____/         ##
-		##       \____/_____/ \___/_____/___/_____/__/_____/_/ |_/_____/         ##
+; definition of words:
+; définition des mots:
+
+; french word	;Rebol word
+; mot français	;mot Rebol
+ecrit:		:print
+boucle:		:loop
+pour:		:for                                         
+raboute:	:rejoin
+
+
+; some testing:
+pour i 1 5 1 [ecrit raboute ["Voilà la boucle #: " i]]
 

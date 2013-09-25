d4775ded7daf4add599a1c34683d2e70519f00c1
pierrechtux <pierrechevaliergeol@gmail.com>
2013-09-25 23:39:54 +0200

    Update frebol.r

--- frebol.r
+++ frebol.r
@@ -5,12 +5,12 @@ rebol []
 
 ; french word	;Rebol word
 ; mot français	;mot Rebol
-ecrit:		:print
+écrit:		:print
 boucle:		:loop
 pour:		:for                                         
 raboute:	:rejoin
 
 
 ; some testing:
-pour i 1 5 1 [ecrit raboute ["Voilà la boucle numéro: " i]]
+pour i 1 5 1 [écrit raboute ["Voilà la boucle numéro: " i]]
 
